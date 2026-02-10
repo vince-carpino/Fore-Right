fa34ba1a51321882048a7241c746aa5cc0070bb3
Vince Carpino <vcarpino33@gmail.com>
2026-02-10 12:39:27 -0800

    Initial Commit

---
+++ Fore RightTests/Fore_RightTests.swift
@@ -0,0 +1,10 @@
+import Testing
+@testable import Fore_Right
+
+struct Fore_RightTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
