f583b5e7ce1651769985573ab0ece43ca74eaf98
Vince Carpino <vcarpino33@gmail.com>
2026-02-12 14:28:29 -0800

    Clean up NewRoundView

--- Fore Right/Views/NewRoundView.swift
+++ Fore Right/Views/NewRoundView.swift
@@ -2,27 +2,27 @@ import SwiftUI
 
 struct NewRoundView: View {
     @State private var date: Date = Date()
-    @State private var showNewCourseModel: Bool = false
+    @State private var showingSheet: Bool = false
 
     var body: some View {
         Text("New Round")
             .font(.largeTitle)
             .bold()
+            .padding()
 
         Text("Course")
             .padding()
         Button {
             print("add new course")
-            showNewCourseModel = true
+            showingSheet = true
         } label: {
             Text("Add New Course")
                 .padding()
-        }.sheet(isPresented: $showNewCourseModel) {
+        }.sheet(isPresented: $showingSheet) {
             NewCourseView()
         }
+        .border(.gray)
 
-        Text("Date")
-            .padding()
         DatePicker(
             "Date",
             selection: $date,
@@ -34,7 +34,9 @@ struct NewRoundView: View {
             print("next")
         } label: {
             Text("Next")
+                .padding()
         }
+        .border(.gray)
     }
 }
 
