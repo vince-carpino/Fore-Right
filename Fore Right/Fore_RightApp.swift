038ca9c38cb48826c41fdddb4bd5f15399c0c195
Vince Carpino <vcarpino33@gmail.com>
2026-02-13 12:56:12 -0800

    Simplify app SwiftData setup

--- Fore Right/Fore_RightApp.swift
+++ Fore Right/Fore_RightApp.swift
@@ -1,27 +1,12 @@
-import SwiftUI
 import SwiftData
+import SwiftUI
 
 @main
 struct Fore_RightApp: App {
-    var sharedModelContainer: ModelContainer = {
-        let schema = Schema([
-            Round.self,
-            Course.self,
-            Hole.self,
-        ])
-        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
-
-        do {
-            return try ModelContainer(for: schema, configurations: [modelConfiguration])
-        } catch {
-            fatalError("Could not create ModelContainer: \(error)")
-        }
-    }()
-
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
-        .modelContainer(sharedModelContainer)
+        .modelContainer(for: Round.self)
     }
 }
