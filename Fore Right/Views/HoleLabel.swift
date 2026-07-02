a80580dd7be00e5336b939b821f81abb36a50f50
Vince Carpino <vcarpino33@gmail.com>
2026-07-02 12:51:47 -0700

    Add color to par 3 and par 5 holes for better visual grepping

--- Fore Right/Views/HoleLabel.swift
+++ Fore Right/Views/HoleLabel.swift
@@ -13,9 +13,22 @@ struct HoleLabel: View {
 struct ParLabel: View {
     var hole: Hole
 
+    private var parColor: Color {
+        switch hole.par {
+        case 3: return .blue
+        case 4: return .primary
+        default: return .orange
+        }
+    }
+
     var body: some View {
-        Text("Par")
-            .textCase(.uppercase)
-        Image(systemName: hole.parIcon)
+        Group {
+            Text("Par")
+                .textCase(.uppercase)
+            Image(systemName: hole.parIcon)
+                .contentTransition(.symbolEffect(.replace))
+        }
+        .foregroundStyle(parColor)
+        .animation(.easeInOut(duration: 0.15), value: hole.par)
     }
 }
